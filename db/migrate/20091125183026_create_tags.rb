2e971b000deb10cba962defb67ff9ab0481cdc3d
Wei Jen Lu <weijenlu@gmail.com>
2009-11-26 02:43:38 +0800

    fix tag's migration

--- db/migrate/20091125183026_create_tags.rb
+++ db/migrate/20091125183026_create_tags.rb
@@ -1,11 +1,11 @@
 class CreateTags < ActiveRecord::Migration
   def self.up
     create_table :tags do |t|
-      t.integer :group_id
-      t.integer :user_id
-      t.string :name
-      t.boolean :is_income
-
+      t.integer :group_id, :null => false
+      t.integer :user_id, :null => false
+      t.string  :name, :null => false
+      t.boolean :is_income, :defaule => false
+      t.integer :used_count, :default => 0
       t.timestamps
     end
   end
