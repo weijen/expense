eac378983244a8b0d4c9d2bf1ac47ecbd734d136
Wei Jen Lu <weijenlu@gmail.com>
2009-11-25 20:20:53 +0800

    fix column name in expenses

--- db/migrate/20091125050932_create_expenses.rb
+++ db/migrate/20091125050932_create_expenses.rb
@@ -6,7 +6,7 @@ class CreateExpenses < ActiveRecord::Migration
       t.integer :tag_id
       t.boolean :is_income, :default => false
       t.float :amount, :null => false
-      t.string :commit
+      t.string :comment
       t.date :charge_date
       t.integer :currency_id
 
