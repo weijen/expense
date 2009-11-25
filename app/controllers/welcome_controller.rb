b47800ab61106fbcdfaea4ad1c70a5c6a3120fe4
Wei Jen Lu <weijenlu@gmail.com>
2009-11-25 21:25:32 +0800

    show recent 20 expenses in dashboard

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,7 @@ class WelcomeController < ApplicationController
   before_filter :login_required
 
   def index
+    @expenses = Expense.find(:all, :conditions => ["group_id IN (?)", @current_user.groups.map { |g| g.id}], :order => "id DESC", :limit => 20) 
   end
 
 end
