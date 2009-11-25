1e1ffd103fa10d2ca97a9fb2466acd0fda8b40a2
Wei Jen Lu <weijenlu@gmail.com>
2009-11-25 22:34:18 +0800

    add group_expenses controller add recent 20 expenses in group show

---
+++ app/controllers/group_expenses_controller.rb
@@ -0,0 +1,14 @@
+class GroupExpensesController < ApplicationController
+  def new
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def delete
+  end
+
+end
---
+++ spec/controllers/group_expenses_controller_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+describe GroupExpensesController do
+
+  #Delete these examples and add some real ones
+  it "should use GroupExpensesController" do
+    controller.should be_an_instance_of(GroupExpensesController)
+  end
+
+
+  describe "GET 'new'" do
+    it "should be successful" do
+      get 'new'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'index'" do
+    it "should be successful" do
+      get 'index'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'edit'" do
+    it "should be successful" do
+      get 'edit'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'delete'" do
+    it "should be successful" do
+      get 'delete'
+      response.should be_success
+    end
+  end
+end
