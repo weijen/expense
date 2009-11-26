b61d01f4d6773cae5fc9db6a4182bcb489d9f13b
Wei Jen Lu <weijenlu@gmail.com>
2009-11-26 19:06:20 +0800

    fix hierarchy to store locale's files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
 
   def group_owner_required
     unless @current_user.manager?(@group)
-      error_stickie "You don't have the right to edit this group"
+      error_stickie t(:have_no_right_to)
       redirect_to "/"
       return false
     end
@@ -18,7 +18,7 @@ class ApplicationController < ActionController::Base
 
   def group_follower_required
     unless @current_user.proven?(@group)
-      error_stickie "You don't have the right to edit this group"
+      error_stickie t(:have_no_right_to)
       redirect_to "/"
       return false
     end
@@ -28,13 +28,13 @@ class ApplicationController < ActionController::Base
   before_filter :set_locale if @current_user
   def set_locale
     # if params[:locale] is nil then I18n.default_locale will be used
-    I18n.locale = @current_user.locale
+    I18n.locale = params[:locale]
     logger.debug "* Locale set to '#{I18n.locale}'"
   end
 
   def default_url_options(options={})
     logger.debug "default_url_options is passed options: #{options.inspect}\n"
-    { :locale => I18n.locale }
+    { :locale => @current_user ? @current_user.locale : I18n.locale }
   end
 
 end
--- config/environment.rb
+++ config/environment.rb
@@ -36,8 +36,8 @@ Rails::Initializer.run do |config|
   config.time_zone = 'UTC'
 
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-  config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}')]
+  config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
   config.i18n.default_locale = "zh-TW"
 end
 
-SITE_NAME = "團體記帳0.0.1版 | 請小心輕放"
+SITE_NAME = "Group Expenses"
