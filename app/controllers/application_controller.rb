9ad8d473de386f144f51d17ca6d53a67496c15cc
Wei Jen Lu <weijenlu@gmail.com>
2009-11-26 17:23:10 +0800

    add I18n infrastructure

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,17 @@ class ApplicationController < ActionController::Base
       return false
     end
     return true
+  end
 
+  before_filter :set_locale
+  def set_locale
+    # if params[:locale] is nil then I18n.default_locale will be used
+    I18n.locale = params[:locale]
+  end
+
+  def default_url_options(options={})
+    logger.debug "default_url_options is passed options: #{options.inspect}\n"
+    { :locale => I18n.locale }
   end
 
 end
--- config/environment.rb
+++ config/environment.rb
@@ -36,8 +36,8 @@ Rails::Initializer.run do |config|
   config.time_zone = 'UTC'
 
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-  # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
-  # config.i18n.default_locale = :de
+  config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}')]
+  config.i18n.default_locale = "zh-TW"
 end
 
 SITE_NAME = "團體記帳"
