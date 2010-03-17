4a8c15447d921ad62fb117e5ba1dbff07695715f
Wei Jen Lu <weijenlu@gmail.com>
2010-03-17 23:58:51 +0800

    fix datatype for PostgreSQL

--- db/migrate/20100312025017_change_tags_default_value.rb
+++ db/migrate/20100312025017_change_tags_default_value.rb
@@ -1,6 +1,6 @@
 class ChangeTagsDefaultValue < ActiveRecord::Migration
   def self.up
-    change_column_default :tags, :is_income, :false
+    change_column_default :tags, :is_income, "f" 
   end
 
   def self.down
